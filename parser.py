e43c16b5f85294a241535558ac430f0ac91487c2
George Yiu <yiu.george@gmail.com>
2014-04-12 01:55:19 -0700

    allow more flexible parsing

--- parser.py
+++ parser.py
@@ -10,14 +10,16 @@ def parse(schema):
         <list>      : denotes a subgroup that will be repeated starting
                       from that line. An empty list defaults as [str], returning
                       the line as a string.
+        <int>       : used to denote the number of repetitions of the subgroup
+                      (known by user, not denoted in the text itself)
 
-    Example: [(), [(), float, [lambda x: x.split()]]]
+    Example: [(), [(), float, [split(int)]]]
     This is the expected line format of the file:
         num_tests
             num_subtests
-            some_float
-                split_line
-                split_line
+            <float>
+                [<int>, <int>, ...]
+                [<int>, <int>, ...]
                 ...
             ...
     '''
@@ -27,16 +29,26 @@ def parse(schema):
 
 def grouper(lines, schema):
     ''' Parse schema by consuming lines'''
-    if () not in schema:
+    countint = [isinstance(x, int)  for x in schema]
+    countparse = [isinstance(x, tuple) for x in schema]
+    if not any(countint) and not any(countparse):
         my_lines = [lines.pop(0) for _ in range(len(schema))]
         result = [x[0](x[1]) for x in zip(schema, my_lines)]
         return result[0] if len(result) == 1 else result
-    index = schema.index(())
-    count = int(lines[index])
-    schema[index] = int
+    if any(countint):
+        idx = countint.index(True)
+        count = schema.pop(idx)
+    else:
+        idx = countparse.index(True)
+        count = int(lines[idx])
+        schema[idx] = int
     sub_schema = schema.pop()
     if len(sub_schema) == 0: sub_schema = [str]
-    my_lines = [lines.pop(0) for i in range(len(schema))]
+    my_lines = [lines.pop(0) for _ in range(len(schema))]
     level = [x[0](x[1]) for x in zip(schema, my_lines)]
     cases = [grouper(lines, sub_schema[:]) for _ in range(count)]
-    return level + [cases]
+    z = level + [cases]
+    return z[0] if len(z) == 1 else z
+
+def split(f):
+    return lambda line: [f(x) for x in line.split()]
--- template.py
+++ template.py
@@ -1,13 +1,13 @@
-from parser import parse
+from parser import parse, split
 
 def helper(test):
-	# solution code here
-	return test
+    # solution code here
+    return test
 
 # schema here
 schema = [(),[]]
 
-num_tests,tests = parse(schema)
+num_tests, tests = parse(schema)
 for case,test in enumerate(tests):
-	sol = helper(test)
-	print 'Case #{}: {}'.format(case+1, sol)
+    sol = helper(test)
+    print 'Case #{}: {}'.format(case+1, sol)
